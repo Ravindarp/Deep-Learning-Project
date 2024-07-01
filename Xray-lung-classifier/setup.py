7e41b926978dbc3b12150565aaf120cdf411b342
Ravindar <104374423+Ravindarp@users.noreply.github.com>
2024-07-01 22:01:04 +0530

    Add files via upload

---
+++ Xray-lung-classifier/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'xray',
+    version= '0.0.0',
+    author= 'Ravindar',
+    author_email= 'ravindarpammi187@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
